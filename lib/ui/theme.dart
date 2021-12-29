4a6dfeac11719a92518aa548b440c659f4bfadfa
Raine M. Ekman <raine@iki.fi>
2021-12-29 11:47:42 +0200

    Split the code a bit, not quite as far as some would, but this is going to be a one screen app for now.

---
+++ lib/ui/theme.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+ThemeData appTheme() {
+  return ThemeData(
+    primarySwatch: Colors.blue,
+
+    /* primaryColor: Colors.white,
+    accentColor: Colors.orange,
+    hintColor: Colors.white,
+    dividerColor: Colors.white,
+    buttonColor: Colors.white,
+    scaffoldBackgroundColor: Colors.black,
+    canvasColor: Colors.black, */
+  );
+}
