5b82bf3d77d20f0f31782bb999d89fea3b2a1cca
Raine M. Ekman <raine@iki.fi>
2021-12-31 09:30:26 +0200

    Getting closer. Business logic almost works.

--- lib/services/scraper.dart
+++ lib/services/scraper.dart
@@ -1,31 +1,111 @@
 import 'dart:async';
+import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 
 class CoinGeckoConnector {
-  // String _urlBase = "https://api.coingecko.com/api/v3/coins/";
-
-  // bitcoin/market_chart/range?vs_currency=eur&from=1392577232&to=1422577232
+  final String _uriAuthorityBase = "api.coingecko.com";
+  final String _uriPath = "api/v3";
 
   // Convert the date part of a DateTime to midnight UTC
-  DateTime midnightUTC(DateTime dt) {
+  DateTime _midnightUTC(DateTime dt) {
     return DateTime.utc(dt.year, dt.month, dt.day);
   }
 
-  // Gets data from CoinGecko
+  // Expect an array of [timestamp, value] arrays.
+  // Assume timestamps are in ascending order and milliseconds.
+  // Return list data normalized to closest UTC midnight
+  // FIX: first and last day are cut off??
+  List _normalizeList(List data) {
+    List out = [];
+    DateTime t0 = DateTime.fromMillisecondsSinceEpoch(data[0][0], isUtc: true);
+    DateTime midnight = _midnightUTC(t0);
+    if (t0.hour >= 12) {
+      // In the PM we're closer to next midnight
+      midnight = _midnightUTC(t0.add(Duration(days: 1)));
+    }
+    Duration delta0 = t0.difference(midnight);
+
+    for (int i = 1; i < data.length; i++) {
+      DateTime t = DateTime.fromMillisecondsSinceEpoch(data[i][0], isUtc: true);
+      if (t.difference(midnight).abs() < t0.difference(midnight).abs()) {
+        // Go on as long as the difference is shrinking
+        t0 = t;
+      } else {
+        // The previous time was closest to midnight
+        out.add([midnight.millisecondsSinceEpoch, data[i - 1][1]]);
+        midnight = _midnightUTC(t.add(Duration(days: 1)));
+        t0 = t;
+      }
+    }
+
+    return out;
+  }
+
+  // Get JSON off the server.
+  // TODO: error handling...
+  Future<String> _httpFetch(String cmd, String? param, String? subcmd,
+      Map<String, dynamic>? query) async {
+    String uri = _uriPath +
+        "/" +
+        cmd +
+        (param != null ? "/" + param : "") +
+        (subcmd != null ? "/" + subcmd : "");
+    http.Response response = await http.get(
+        Uri.https(_uriAuthorityBase, uri, query),
+        headers: {"Accept": "application/json"});
+    print("Path: ${uri}");
+    print("Query: ${query}");
+
+    return response.body;
+  }
+
+  // Gets data, parses JSON and stores it
   Future<String> getMarketChartRange(
       String coin, String vs_currency, DateTimeRange when) async {
-    // The DateTimeRange probably is in the local time zone
-    int t1 = (midnightUTC(when.start).millisecondsSinceEpoch / 1000).floor();
-    int t2 = (midnightUTC(when.end).millisecondsSinceEpoch / 1000).floor();
+    // The DateTimeRange is in the local time zone
+    int t1 = (_midnightUTC(when.start).millisecondsSinceEpoch / 1000).floor();
+    int t2 = (_midnightUTC(when.end).millisecondsSinceEpoch / 1000).floor();
 
-    http.Response response = await http.get(Uri.https(
-        "api.coingecko.com", "api/v3/coins/" + coin + "/market_chart/range", {
+    String json = await _httpFetch("coins", coin, "market_chart/range", {
       "from": t1.toString(),
       "to": t2.toString(),
       "vs_currency": vs_currency
-    }));
-    // print("Response ${response.body.toString()}");
-    return response.body;
+    });
+    var data = jsonDecode(json);
+    //print(json);
+
+    // How many days is the longest bearish (downward) trend within a given date range?
+    var prices = _normalizeList(data["prices"]);
+    List bearish = [prices[0][0], 0]; // Start date, duration
+    int bearCount = 0;
+    int i;
+    for (i = 1; i < prices.length; i++) {
+      print(
+          "${DateTime.fromMillisecondsSinceEpoch(prices[i][0], isUtc: true)}: ${prices[i][1]}");
+      if (prices[i][1] < prices[i - 1][1]) {
+        bearCount++;
+      }
+
+      if (prices[i][1] >= prices[i - 1][1]) {
+        if (bearCount > bearish[1]) {
+          bearish = [prices[i - bearCount][0], bearCount];
+        }
+        bearCount = 0;
+      }
+    }
+    // In case we end in a bearish period
+    if (bearCount > bearish[1]) {
+      bearish = [prices[i - bearCount][0], bearCount];
+    }
+
+    // var marketCaps = _normalizeList(data["market_caps"]);
+    // Which date within a given date range had the highest trading volume?
+    var totalVolumes = _normalizeList(data["total_volumes"]);
+    List maxVolume = totalVolumes[0];
+    totalVolumes.forEach((entry) => {
+          if (entry[1] > maxVolume[1]) {maxVolume = entry}
+        });
+    return "${DateTime.fromMillisecondsSinceEpoch(bearish[0], isUtc: true)}: ${bearish[1]}";
   }
 }
--- lib/ui/screen.dart
+++ lib/ui/screen.dart
@@ -14,8 +14,8 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   DateTimeRange? _when;
   String _dateRange = "Press the button to set date range.";
-  final String _bearish = "";
-  final String _highestVolume = "";
+  String _bearish = "";
+  String _highestVolume = "";
   String _optimalDates = "";
   CoinGeckoConnector conn = CoinGeckoConnector();
 
@@ -40,21 +40,21 @@ class _MyHomePageState extends State<MyHomePage> {
         _when = pickedRange;
         _dateRange =
             _toDateString(_when!.start) + " - " + _toDateString(_when!.end);
-        _optimalDates = response.substring(0, 40) + " ...";
+
+        _optimalDates = response;
       });
     }
   }
 
+  // Invoke "debug painting" (press "p" in the console, choose the
+  // "Toggle Debug Paint" action from the Flutter Inspector in Android
+  // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+  // to see the wireframe for each widget.
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called.
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
@@ -62,19 +62,8 @@ class _MyHomePageState extends State<MyHomePage> {
         // in the middle of the parent.
         child: Column(
           // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
+          // arranges them vertically.
+
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
